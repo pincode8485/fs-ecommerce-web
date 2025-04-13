6ad73b1fc8ed24df5a4bacf67f5f84a2f8b41fee
pincode8485 <miftahfaridl.adam@gmail.com>
2025-04-13 15:00:27 +0700

    fix inngest.js errors

--- config/inngest.js
+++ config/inngest.js
@@ -12,10 +12,10 @@ export const syncUserCreation = inngest.createFunction(
     },
     {event: 'clerk/user.created'},
     async ({event}) => {
-        const { id, firsrt_name, last_name, email_adresses, image_url } = event.data 
+        const { id, first_name, last_name, email_addresses, image_url } = event.data 
         const userData = {
             _id:id,
-            email: email_adresses[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image_url: image_url
         }
@@ -31,10 +31,10 @@ export const syncUserUpdation = inngest.createFunction(
     },
     { event: 'clerk/user.updated' },
     async ({event}) => {
-        const { id, firsrt_name, last_name, email_adresses, image_url } = event.data 
+        const { id, first_name, last_name, email_addresses, image_url } = event.data 
         const userData = {
             _id:id,
-            email: email_adresses[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image_url: image_url
         }
